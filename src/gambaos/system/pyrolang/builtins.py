fcaf1af9b3f561b9302500134151c553603e9bad
Jurre Kruiper <24962@carmelcollegesalland.nl>
2026-08-27 23:12:13 +0200

    fix: restore mistakenly deleted Builtins module

---
+++ src/gambaos/system/pyrolang/builtins.py
@@ -0,0 +1,31 @@
+# Built-in mathematical functions
+## These functions are translated and parsed through Pyrolang
+def _equals(a, b):
+    return a == b
+
+def _med(a, b):
+    return (a + b) / 2
+
+def _add(a, b):
+    return a + b
+
+def _subtract(a, b):
+    return a - b
+
+def _multiply(a, b):
+    return a * b
+
+def _divide(a, b):
+    return a / b
+
+# Quite useless builtins
+## Those can be used in some cases to do things the difficult way
+def _switch(i: int):
+    if i == 0:
+        return 1
+    else:
+        return 0
+
+# Print output
+def _out(i: str):
+    print(i)
